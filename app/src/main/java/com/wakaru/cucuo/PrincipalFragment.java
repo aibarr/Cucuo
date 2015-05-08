07f7fbb56e962baffc0febcfde823a93b31b3e8e
aibarr <alfredo.barra@usach.cl>
2015-05-08 15:46:27 -0300

    Initial commit

---
+++ app/src/main/java/com/wakaru/cucuo/PrincipalFragment.java
@@ -0,0 +1,41 @@
+package com.wakaru.cucuo;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PrincipalFragment extends Fragment {
+
+    Button simulCompra;
+
+    public PrincipalFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_principal, container, false);
+
+        simulCompra = (Button)root.findViewById(R.id.simulCompra);
+
+        simulCompra.setOnClickListener(new Button.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v){
+                Intent aSimular = new Intent(getActivity(), simularCompra.class);
+                startActivity(aSimular);
+            }
+
+        });
+
+        return root;
+    }
+}
