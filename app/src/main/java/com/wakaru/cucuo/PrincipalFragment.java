e691c46c0d76b55284ce3733aee0a7db887ee412
joel <joelav_3@hotmail.com>
2015-05-09 21:20:31 -0300

    asd

--- app/src/main/java/com/wakaru/cucuo/PrincipalFragment.java
+++ app/src/main/java/com/wakaru/cucuo/PrincipalFragment.java
@@ -1,12 +1,13 @@
 package com.wakaru.cucuo;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 
 
 /**
--- app/src/main/java/com/wakaru/cucuo/simularCompraFragment.java
+++ app/src/main/java/com/wakaru/cucuo/simularCompraFragment.java
@@ -1,10 +1,13 @@
 package com.wakaru.cucuo;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 
 /**
@@ -12,12 +15,23 @@ import android.view.ViewGroup;
  */
 public class simularCompraFragment extends Fragment {
 
+    private SimpleAdapter mSchedule;
+
     public simularCompraFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_simular_compra, container, false);
+
+        View root2 = inflater.inflate(R.layout.fragment_simular_compra, container, false);
+
+        String[] mytems = {"CMR","BCI","BBVA","Falabella","Tarjeta4","Tarjeta5","Tarjeta6"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(root2.getContext(), android.R.layout.simple_list_item_1, mytems);
+        ListView list = (ListView) root2.findViewById(R.id.listView1);
+        list.setAdapter(adapter);
+
+        return root2;
     }
+
 }
