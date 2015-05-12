31c9ae023104542491b36949ef01152b2b6d486a
joel <joelav_3@hotmail.com>
2015-05-12 11:02:10 -0300

    Hola

--- app/src/main/java/com/wakaru/cucuo/simularCompraFragment.java
+++ app/src/main/java/com/wakaru/cucuo/simularCompraFragment.java
@@ -5,8 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 
@@ -25,11 +23,6 @@ public class simularCompraFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View root2 = inflater.inflate(R.layout.fragment_simular_compra, container, false);
-
-        String[] mytems = {"CMR","BCI","BBVA","Falabella","Tarjeta4","Tarjeta5","Tarjeta6"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(root2.getContext(), android.R.layout.simple_list_item_1, mytems);
-        ListView list = (ListView) root2.findViewById(R.id.listView1);
-        list.setAdapter(adapter);
 
         return root2;
     }
