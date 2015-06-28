8a375f748458bddc814266b965b474313ab599d9
Joel <joelav_3@hotmail.com>
2015-06-28 17:04:41 -0400

    CuCuo Final V:2.2 -Agregado tamaño maximo de 10 caracteres para el nombre de la compra.

--- app/src/main/java/com/wakaru/cucuo/DialogoCompra.java
+++ app/src/main/java/com/wakaru/cucuo/DialogoCompra.java
@@ -3,11 +3,14 @@ package com.wakaru.cucuo;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -19,6 +22,8 @@ public class DialogoCompra extends DialogFragment implements View.OnClickListene
     Button ButtonCancelarCompra;
     EditText EditTextNombreCompra;
     Comunicator comunicator;
+    TextView TextViewMaxCaracteres;
+    int maxCaracteres, actualCaracteres;
 
     @Override
     public void onAttach(Activity activity) {
@@ -33,17 +38,46 @@ public class DialogoCompra extends DialogFragment implements View.OnClickListene
         ButtonAceptarComprar = (Button) view.findViewById(R.id.ButtonAceptarComprar);
         ButtonCancelarCompra = (Button) view.findViewById(R.id.ButtonCancelarCompra);
         EditTextNombreCompra = (EditText) view.findViewById(R.id.EditTextNombreCompra);
+        TextViewMaxCaracteres = (TextView) view.findViewById(R.id.TextViewMaxCaracteres);
+        maxCaracteres = 10;
 
         ButtonAceptarComprar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (EditTextNombreCompra.getText().toString().equals("")) {
-                    Toast.makeText(getActivity().getApplicationContext(), "Ingresa nombre", Toast.LENGTH_SHORT).show();
+                if (EditTextNombreCompra.getText().toString().equals("") || (actualCaracteres > maxCaracteres)) {
+
+                    if (EditTextNombreCompra.getText().toString().equals("")){
+                        Toast.makeText(getActivity().getApplicationContext(), "Ingresa nombre", Toast.LENGTH_SHORT).show();
+                    }
+                    if ((actualCaracteres > maxCaracteres)){
+                        Toast.makeText(getActivity().getApplicationContext(), "Nombre muy largo", Toast.LENGTH_SHORT).show();
+                    }
 
                 } else {
                     comunicator.onDialogMessage(EditTextNombreCompra.getText().toString());
                     dismiss();
                 }
+            }
+        });
+
+        EditTextNombreCompra.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                actualCaracteres = EditTextNombreCompra.length();
+                TextViewMaxCaracteres.setText(String.valueOf(actualCaracteres) + " / " + String.valueOf(maxCaracteres));
+
+                TextViewMaxCaracteres.setTextColor(revCaracteres(actualCaracteres, maxCaracteres));
             }
         });
 
@@ -56,6 +90,22 @@ public class DialogoCompra extends DialogFragment implements View.OnClickListene
 
         setCancelable(false);
         return view;
+    }
+
+    /**
+     * Funcion que retorna el color del texto de acuerdo a la cantidad de caracteres introducidas
+     * @param actualCaracteres cantidad de caracteres escritos
+     * @param maxCaracteres cantidad maxima de caracteres soportados
+     * @return retorna el color (negro si no se pasa el maximo, rojo si se pasa)
+     */
+    private int revCaracteres(int actualCaracteres, int maxCaracteres) {
+
+        if (actualCaracteres > maxCaracteres){
+            return getResources().getColor(R.color.Saldo_No_Disponible);
+        }
+        else{
+            return getResources().getColor(R.color.Saldo_Disponible);
+        }
     }
 
     interface Comunicator {
